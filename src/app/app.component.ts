81b9f2f68a1274bb55db4bcb5b9ebd232719a4da
Phyo Thet Pai <135086719+PhyoTP@users.noreply.github.com>
2024-04-01 23:08:58 +0800

    added tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TabComponent } from './tab/tab.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TabComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/tab/tab.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab',
+  standalone: true,
+  imports: [],
+  template: `
+    <iframe src="https://phyotp.github.io"></iframe>
+  `,
+  styleUrl: './tab.component.css'
+})
+export class TabComponent {
+
+}
