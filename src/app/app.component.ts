30a87fbc2a0bd13101a457fac2de7d4c919347bd
Phyo Thet Pai <135086719+PhyoTP@users.noreply.github.com>
2024-04-01 23:31:54 +0800

    tab bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TabComponent } from './tab/tab.component';
+import { TabBarComponent } from './tab-bar/tab-bar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TabComponent],
+  imports: [RouterOutlet, TabComponent,TabBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/tab-bar/tab-bar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-bar',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      tab-bar works!
+    </p>
+  `,
+  styleUrl: './tab-bar.component.css'
+})
+export class TabBarComponent {
+
+}
