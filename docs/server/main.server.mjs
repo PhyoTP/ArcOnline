f81a26303cf8045b538d8d740eda30d4ee1eb177
Phyo Thet Pai <135086719+PhyoTP@users.noreply.github.com>
2024-04-17 13:16:03 +0000

    oops forgot to add buttons also icon and changed color and did a lot of css

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-QD2YRUCK.mjs";import"./chunk-KIJT2WL3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-QNPTWSJW.mjs";import"./chunk-KIJT2WL3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
