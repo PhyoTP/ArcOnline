1c0906903a5a7e55c97b9e11c810bf4725fc8828
Phyo Thet Pai <135086719+PhyoTP@users.noreply.github.com>
2024-07-13 07:41:10 +0000

    it works i hope

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-O4ZXF6C3.mjs";import"./chunk-KIJT2WL3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-JWCNPV6X.mjs";import"./chunk-KIJT2WL3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
