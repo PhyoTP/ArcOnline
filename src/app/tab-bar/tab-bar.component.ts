f45255a828011af0b35bda47ff1f92d6b867677d
Phyo Thet Pai <135086719+PhyoTP@users.noreply.github.com>
2024-07-13 12:31:39 +0000

    textbox

--- src/app/tab-bar/tab-bar.component.ts
+++ src/app/tab-bar/tab-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { url } from '../../url';
 @Component({
   selector: 'app-tab-bar',
   standalone: true,
@@ -7,12 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: "./tab-bar.component.html",
   styleUrl: './tab-bar.component.css'
 })
-export class TabBarComponent {
-  url = "arc.net"
-  close(){
-    window.location.href = "about:blank"
+  export class TabBarComponent {
+    link = url[0];
+
+    constructor() {
+      // This line is not necessary; removing to avoid confusion
+      // url = this.link;
+    }
+
+    close() {
+      window.location.href = "about:blank";
+    }
+
+    loadUrl() {
+      // Directly modifying the imported variable
+      this.updateUrl(this.link);
+    }
+
+    updateUrl(newUrl: string) {
+      url[0] = newUrl;
+    }
   }
-  loadUrl(){
-    
-  }
-}
--- src/app/tab/tab.component.ts
+++ src/app/tab/tab.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
-
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { url } from '../../url';
 @Component({
   selector: 'app-tab',
   standalone: true,
   imports: [],
   template: `
   <div class="iframe-container">
-    <iframe src="https://arc.net"></iframe>
+    <iframe [src]="safeLink"></iframe>
   </div>
   `,
-  styleUrl: './tab.component.css'
+  styleUrls: ['./tab.component.css']
 })
 export class TabComponent {
+  link: string = url[0];
+  safeLink: SafeResourceUrl;
 
+  constructor(private sanitizer: DomSanitizer) {
+    this.safeLink = this.sanitizer.bypassSecurityTrustResourceUrl(this.link);
+  }
 }
