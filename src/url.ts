b4dc6912862ad6ee51c1bf4fc30ad9c4b56fee4a
Phyo Thet Pai <135086719+PhyoTP@users.noreply.github.com>
2024-07-17 12:00:01 +0000

    doing my own thing

--- src/url.ts
+++ src/url.ts
@@ -1,1 +1,30 @@
-export const url: string[] = ["https://arc.net"]
+export interface Url {
+    url: string,
+    website: string
+}
+function checkURL(link: string): Url {
+    if (!link.startsWith('http://') && !link.startsWith('https://')) {
+        return { url: `https://${link}`, website: link };
+    }else {
+        if (link.startsWith("http://")) {
+            return { url: link, website: link.slice(7) };
+        }else {
+            return { url: link, website: link.slice(8) };
+        }
+    }
+}
+export interface Tab {
+    titles: string[], 
+    links: Url[]
+}
+function makeTab(sites: string[]): Tab{
+    let thing: Tab = {titles: [], links: []};
+    for (let site of sites) {
+        thing.links.push(checkURL(site));
+        thing.titles.push(checkURL(site).website);
+    }
+    return thing;
+}
+export const currentTab: Tab = makeTab(["arc.net"]);
+export const pinnedTabs: Tab[] = [makeTab(["arc.net"]),makeTab(["phyotp.github.io"])];
+export const tabs: Tab[] = [makeTab(["github.com","chatgpt.com"]),makeTab(["discord.com"])];
